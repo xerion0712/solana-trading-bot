a27bc842317b4747290f71495d25ddde9422b0ec
ivan-0224 <ivanmiluticev@gmail.com>
2025-01-11 01:41:09 +0900

    unrelease main folder

--- main/callback_handlers/back.py
+++
@@ -1,9 +0,0 @@
-from telegram import Update
-from telegram.ext import CallbackContext
-
-def back(update: Update, context: CallbackContext) -> None:
-    query = update.callback_query
-    query.answer()  # Acknowledge the callback
-    chat_id = update.effective_chat.id
-    message_id = query.message.message_id
-    context.bot.delete_message(chat_id, message_id=message_id)
--- main/utils/__init__.py
+++
@@ -1,3 +0,0 @@
-def append_to_log_file(text):
-    with open('log.txt', 'a') as log_file:
-        log_file.write(text + '\n')
